bb86bd82e74ed61a0949112fce7eb038eab6cd40
frontend-fxs <oriol@fxstreet.com>
2019-01-08 11:55:32 +0100

    up

--- test/login.js
+++ test/login.js
@@ -7,13 +7,19 @@ fixture `Getting Started`
 
 const getPageUrl = ClientFunction(() => window.location.href.toString());
 
-
 test('Login Button exist', async t => {
     await t
         .expect(Selector('#login')).exists;
 });
+
 test('Click Login Button', async t => {
     await t
         .click(Selector('#login'))
         .expect(getPageUrl()).eql('https://www.fxstreet.com/account/login?uriToRedirect=https://www.fxstreet.com/', 'Login Page Loaded', { timeout: 500 });
 });
+
+
+
+
+
+7
