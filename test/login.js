4d53e789933f048b73bf68c492d93544de1563f9
frontend-fxs <oriol@fxstreet.com>
2019-01-08 16:35:12 +0100

    up

--- test/login.js
+++ test/login.js
@@ -2,24 +2,24 @@ import { Selector } from 'testcafe';
 import { ClientFunction } from 'testcafe';
 
 
-fixture `Getting Started`
+fixture `Start Login test`
     .page `https://www.fxstreet.com/`;
 
 const getPageUrl = ClientFunction(() => window.location.href.toString());
+const prestitialContinue = Selector('.fxs_prestitial-continue');
+const LoginButton = Selector('#login');
+const EmailInputField = Selector('[id^=fxs_login_username_login_]');
+const PassInputField = Selector('[id^=fxs_login_password_login_]');
+test('Login test', async t => {
 
-test('Login Button exist', async t => {
     await t
-        .expect(Selector('#login')).exists;
+        .maximizeWindow()
+        .expect(getPageUrl()).eql('https://www.fxstreet.com/', 'Home Page Not Loaded', { timeout: 5000 })
+        .click(prestitialContinue)
+        .expect(LoginButton.exists).ok('Login Button not found')
+        .click(LoginButton)
+        .expect(getPageUrl()).eql('https://www.fxstreet.com/account/login?uriToRedirect=https://www.fxstreet.com/', 'Login Page Not Loaded', { timeout: 5000 })
+        .expect(EmailInputField.exists).ok('Email input field not found')
+        .expect(PassInputField.exists).ok('Email input field not found')
+
 });
-
-test('Click Login Button', async t => {
-    await t
-        .click(Selector('#login'))
-        .expect(getPageUrl()).eql('https://www.fxstreet.com/account/login?uriToRedirect=https://www.fxstreet.com/', 'Login Page Loaded', { timeout: 500 });
-});
-
-
-
-
-
-7
